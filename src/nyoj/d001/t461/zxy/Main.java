5f1ca5f10c1d890ded37bbc6f83ec0db05122899
zhangxy3 <zhangxy3@mochasoft.com.cn>
2018-02-08 15:35:04 +0800

    t461

--- src/nyoj/d001/t461/zxy/Main.java
+++ src/nyoj/d001/t461/zxy/Main.java
@@ -1,7 +1,22 @@
-package nyoj.day001_t461.zxy;
+package nyoj.d001.t461.zxy;
+
+import java.util.Scanner;
 
 /**
  * Created by zhang on 2018-02-08.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int sum = (int) ((Math.pow((1 + Math.sqrt(5)) / 2, n) - Math.pow((1 - Math.sqrt(5)) / 2, n)) / Math.sqrt(5));
+
+            while (sum > 9999) {
+                sum /= 10;
+            }
+            System.out.println(sum);
+        }
+    }
 }
