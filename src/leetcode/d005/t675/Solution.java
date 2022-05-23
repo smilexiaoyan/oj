c214941a1ed1d2df7e7176f9c28f654bcf5fe262
zhangxiaoyan <zhangxiaoyan@techfin.ai>
2022-05-23 16:12:12 +0800

    为高尔夫比赛砍树

---
+++ src/leetcode/d005/t675/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.d005.t675;
+
+import java.util.List;
+
+class Solution {
+    public int cutOffTree(List<List<Integer>> forest) {
+
+        return 0;
+    }
+}
---
+++ src/leetcode/d005/t675/node.md
@@ -0,0 +1,3 @@
+[https://leetcode.cn/problems/cut-off-trees-for-golf-event/](https://leetcode.cn/problems/cut-off-trees-for-golf-event/)
+
+#### 675. 为高尔夫比赛砍树
