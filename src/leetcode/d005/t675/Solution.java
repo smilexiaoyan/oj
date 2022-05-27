eea64f38e00b93e05635e329e4adb2d835dc2e55
zhangxiaoyan <zhangxiaoyan@techfin.ai>
2022-05-27 12:21:50 +0800

    为高尔夫比赛砍树 NG

--- src/leetcode/d005/t675/Solution.java
+++ src/leetcode/d005/t675/Solution.java
@@ -1,12 +1,17 @@
 package leetcode.d005.t675;
 
+import leetcode.utils.Utils;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
+
     static class Pos {
-        public final int x;
-        public final int y;
+
+        public int x;
+
+        public int y;
 
         public Pos(int x, int y) {
             this.x = x;
@@ -14,9 +19,14 @@ class Solution {
         }
 
         @Override
+        public String toString() {
+            return "(" + x + ", " + y + ')';
+        }
+
+        @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {return true;}
+            if (!(o instanceof Pos)) {return false;}
             Pos pos = (Pos) o;
             return x == pos.x && y == pos.y;
         }
@@ -27,7 +37,7 @@ class Solution {
         }
     }
 
-    static Map<Pos, Integer> caches = new HashMap<>();
+    Map<Pos, int[][]> asl_map = new HashMap<>();
 
     public int cutOffTree(List<List<Integer>> forest) {
         int m = forest.size();
@@ -43,60 +53,100 @@ class Solution {
             }
         }
         final List<Integer> collect = map.keySet().stream().sorted().collect(Collectors.toList());
+        int[][] asl = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(asl[i], -1);
+        }
+        f(forest, asl, 0, 0, 0);
+        asl_map.put(new Pos(0, 0), asl);
+        f1(asl, 0, 0);
+
         Pos current = new Pos(0, 0);
         int result = 0;
         for (int k : collect) {
             final Pos dst = map.get(k);
-            int cv = forest.get(current.x).get(current.y);
-            int pv = forest.get(dst.x).get(dst.y);
-            Integer cache = caches.get(cv > pv ? new Pos(pv, cv) : new Pos(cv, pv));
-            if (cache != null) {
-                result += cache;
-                current = dst;
-                continue;
-            }
-            int[][] asl = new int[m][n];
-            for (int i = 0; i < m; i++) {
-                Arrays.fill(asl[i], Integer.MAX_VALUE);
-            }
-            Stack<Pos> stack = new Stack<>();
-            f(forest, asl, dst.x, dst.y, 0, stack);
-            if (asl[current.x][current.y] == Integer.MAX_VALUE) {
+            int[][] asl_current = asl_map.get(dst);
+            if (asl_current == null || asl_current[current.x][current.y] < 0) {
                 return -1;
             }
-            result += asl[current.x][current.y];
+            result += asl_current[current.x][current.y];
             current = dst;
         }
         return result;
     }
 
-    void f(List<List<Integer>> forest, int[][] asl, int x, int y, int value, Stack<Pos> stack) {
+    void f(List<List<Integer>> forest, int[][] asl, int x, int y, int value) {
         if (x < 0 || y < 0 || x >= asl.length || y >= asl[0].length) {
             return;
         }
-        final Integer fv = forest.get(x).get(y);
-        if (fv == 0) {
+        if (forest.get(x).get(y) == 0) {
             return;
         }
-        if (value >= asl[x][y]) {
+        if (asl[x][y] >= 0 && value >= asl[x][y]) {
             return;
         }
-        stack.forEach(item -> {
-            final Pos pos;
-            final Integer iv = forest.get(item.x).get(item.y);
-            if (iv > fv) {
-                pos = new Pos(fv, iv);
-            } else {
-                pos = new Pos(iv, fv);
-            }
-            caches.put(pos, value - asl[item.x][item.y]);
-        });
         asl[x][y] = value;
-        stack.push(new Pos(x, y));
-        f(forest, asl, x - 1, y, value + 1, stack);
-        f(forest, asl, x + 1, y, value + 1, stack);
-        f(forest, asl, x, y - 1, value + 1, stack);
-        f(forest, asl, x, y + 1, value + 1, stack);
-        stack.pop();
+
+        f(forest, asl, x - 1, y, value + 1);
+        f(forest, asl, x + 1, y, value + 1);
+        f(forest, asl, x, y - 1, value + 1);
+        f(forest, asl, x, y + 1, value + 1);
     }
+
+    void f1(int[][] asl, int x, int y) {
+        final Pos src = new Pos(x, y);
+
+        f2(asl, src, new Pos(x - 1, y));
+        f2(asl, src, new Pos(x + 1, y));
+        f2(asl, src, new Pos(x, y - 1));
+        f2(asl, src, new Pos(x, y + 1));
+    }
+
+    void f2(int[][] asl, Pos src, Pos dst) {
+        if (outOfBound(asl, dst.x, dst.y)) {
+            return;
+        }
+        if (asl_map.containsKey(dst)) {
+            return;
+        }
+        final int[][] asl0 = asl_map.get(new Pos(0, 0));
+        if (asl0[dst.x][dst.y] <= asl0[src.x][src.y]) {
+            return;
+        }
+        int[][] next_asl = new int[asl.length][asl[0].length];
+        for (int[] ints : next_asl) {
+            Arrays.fill(ints, -1);
+        }
+        f3(asl, next_asl, dst.x, dst.y, asl[src.x][src.y], -1);
+        f3(asl, next_asl, src.x, src.y, -1, 1);
+        asl_map.put(dst, next_asl);
+
+        f2(next_asl, dst, new Pos(dst.x - 1, dst.y));
+        f2(next_asl, dst, new Pos(dst.x + 1, dst.y));
+        f2(next_asl, dst, new Pos(dst.x, dst.y - 1));
+        f2(next_asl, dst, new Pos(dst.x, dst.y + 1));
+    }
+
+    void f3(int[][] asl, int[][] dst_asl, int x, int y, int value, int dist) {
+        if (outOfBound(asl, x, y)) {
+            return;
+        }
+        if (asl[x][y] <= value) {
+            return;
+        }
+        if (dst_asl[x][y] != -1) {
+            return;
+        }
+        dst_asl[x][y] = asl[x][y] + dist;
+
+        f3(asl, dst_asl, x - 1, y, asl[x][y], dist);
+        f3(asl, dst_asl, x + 1, y, asl[x][y], dist);
+        f3(asl, dst_asl, x, y - 1, asl[x][y], dist);
+        f3(asl, dst_asl, x, y + 1, asl[x][y], dist);
+    }
+
+    boolean outOfBound(int[][] asl, int x, int y) {
+        return x < 0 || y < 0 || x >= asl.length || y >= asl[0].length || asl[x][y] < 0;
+    }
+
 }
---
+++ src/leetcode/utils/Utils.java
@@ -0,0 +1,13 @@
+package leetcode.utils;
+
+import java.util.Arrays;
+
+public class Utils {
+
+    public static void print(int[][] a){
+        for(int[] b: a){
+            System.out.println(Arrays.toString(b));
+        }
+        System.out.println();
+    }
+}
